dd1df3b959fc6714abcce09245a6fd60697150e4
Erik Hedberg <erik@hedberg.at>
2018-10-24 18:24:39 +0200

    Reset styling according to Material

--- src/App.jsx
+++ src/App.jsx
@@ -6,22 +6,26 @@ import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import AppBar from '@material-ui/core/AppBar';
 import Typography from '@material-ui/core/Typography';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import {Routes} from './components';
 
 class App extends React.Component {
   render() {
     const savedCycles = [];
     return (
-      <Grid container spacing={24}>
-        <Grid item xs={12}>
-          <AppBar position="static">
-            <Typography variant="h6" color="inherit">
-              Workout templates
-            </Typography>
-          </AppBar>
-          <Routes />
+      <React.Fragment>
+        <CssBaseline />
+        <Grid container spacing={24}>
+          <Grid item xs={12}>
+            <AppBar position="static">
+              <Typography variant="h6" color="inherit">
+                Workout templates
+              </Typography>
+            </AppBar>
+            <Routes />
+          </Grid>
         </Grid>
-      </Grid>
+      </React.Fragment>
     );
   }
 }
