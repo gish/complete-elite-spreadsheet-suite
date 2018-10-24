f3700f389785e7b4a647de9cc0cfc86ab5f47fee
Erik Hedberg <erik@hedberg.at>
2018-10-24 18:16:10 +0200

    Add separate router component

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,27 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import {hot} from 'react-hot-loader';
-import * as cyclesPages from './pages/cycles';
-
-const uuidV4 =
-  '[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}';
+import {withStyles} from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import AppBar from '@material-ui/core/AppBar';
+import Typography from '@material-ui/core/Typography';
+import {Routes} from './components';
 
 class App extends React.Component {
   render() {
     const savedCycles = [];
     return (
-      <Router>
-        <div>
-          <Route
-            exact
-            path="/"
-            render={() => (
-              <div>
-                <Link to="cycles">Cycles</Link>
-                <Link to="cycles/create">Create cycle</Link>
-              </div>
-            )}
-          />
-          <Route exact path="/cycles" component={cyclesPages.List} />
-          <Route exact path="/cycles/create" component={cyclesPages.Create} />
-          <Route
-            exact
-            path={`/cycles/:id(${uuidV4})`}
-            component={cyclesPages.Show}
-          />
-        </div>
-      </Router>
+      <Grid container spacing={24}>
+        <Grid item xs={12}>
+          <AppBar position="static">
+            <Typography variant="h6" color="inherit">
+              Workout templates
+            </Typography>
+          </AppBar>
+          <Routes />
+        </Grid>
+      </Grid>
     );
   }
 }
---
+++ src/components/Routes.jsx
@@ -0,0 +1,39 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import {hot} from 'react-hot-loader';
+
+import * as cyclesPages from '../pages/cycles';
+
+const uuidV4 =
+  '[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}';
+
+class Routes extends React.Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Route
+            exact
+            path="/"
+            render={() => (
+              <div>
+                <Link to="cycles">Cycles</Link>
+                <Link to="cycles/create">Create cycle</Link>
+              </div>
+            )}
+          />
+          <Route exact path="/cycles" component={cyclesPages.List} />
+          <Route exact path="/cycles/create" component={cyclesPages.Create} />
+          <Route
+            exact
+            path={`/cycles/:id(${uuidV4})`}
+            component={cyclesPages.Show}
+          />
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default hot(module)(Routes);
