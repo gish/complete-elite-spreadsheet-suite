b96caa7be5ed55224af5336e55cf92912c2a0c23
Erik Hedberg <erik@hedberg.at>
2018-10-30 08:57:56 +0100

    List cycles using List

--- src/pages/cycles/List/Index.jsx
+++ src/pages/cycles/List/Index.jsx
@@ -2,18 +2,21 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import {connect} from 'react-redux';
 import {Link} from 'react-router-dom';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
 import propTypes from '../../../proptypes';
 
 const CyclesLister = ({cycles, onChoose}) => (
   <div>
     <h1>Saved cycles</h1>
-    <ul>
+    <List component="nav">
       {cycles.map(cycle => (
-        <li key={cycle.name}>
-          <Link to={`/cycles/${cycle.id}`}>{cycle.name}</Link>
-        </li>
+        <ListItem component={Link} to={`/cycles/${cycle.id}`} button>
+          <ListItemText inset primary={cycle.name} />
+        </ListItem>
       ))}
-    </ul>
+    </List>
   </div>
 );
 
