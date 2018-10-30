d685d59b71019fa30765490c73c6ff3ffba7401e
Erik Hedberg <erik@hedberg.at>
2018-10-30 09:05:03 +0100

    Use common header

--- src/pages/cycles/List/Index.jsx
+++ src/pages/cycles/List/Index.jsx
@@ -5,11 +5,14 @@ import {Link} from 'react-router-dom';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
+import Typography from '@material-ui/core/Typography';
 import propTypes from '../../../proptypes';
 
 const CyclesLister = ({cycles, onChoose}) => (
   <div>
-    <h1>Saved cycles</h1>
+    <Typography variant="h4" gutterBottom>
+      Saved cycles
+    </Typography>
     <List component="nav">
       {cycles.map(cycle => (
         <ListItem component={Link} to={`/cycles/${cycle.id}`} button>
