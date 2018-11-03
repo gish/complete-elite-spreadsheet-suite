ac9b8e172c59457286221f134e9b010a5d324d0a
Erik Hedberg <erik@hedberg.at>
2018-11-03 20:45:34 +0100

    Parse empty state

--- src/state/utils/storage.js
+++ src/state/utils/storage.js
@@ -1,5 +1,5 @@
 const KEY = 'state';
 const set = val => window.localStorage.setItem(KEY, JSON.stringify(val));
-const get = () => JSON.parse(window.localStorage.getItem(KEY) || {});
+const get = () => JSON.parse(window.localStorage.getItem(KEY) || '{}');
 
 export default {get, set};
