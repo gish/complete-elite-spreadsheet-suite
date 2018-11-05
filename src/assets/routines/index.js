eda094b1a8cbd50b79c77a8f9ab2d8b0a31c9e8a
Erik Hedberg <erik@hedberg.at>
2018-11-05 13:01:06 +0100

    Add Russian Squat Routine

--- src/assets/routines/index.js
+++ src/assets/routines/index.js
@@ -1,7 +1,13 @@
 import fiveThreeOne from './531.json';
 import nsunsBench from './nsuns-bench.json';
 import makeMommaProud from './make-momma-proud.json';
+import russianSquatRoutine from './russian-squat-routine.json';
 
-const routines = [fiveThreeOne, nsunsBench, makeMommaProud];
+const routines = [
+  fiveThreeOne,
+  nsunsBench,
+  makeMommaProud,
+  russianSquatRoutine,
+];
 
 export default routines;
