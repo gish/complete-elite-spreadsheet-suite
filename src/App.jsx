c7504747f6a9ccccf5f7860ee9415c057e192cdb
Erik Hedberg <erik@hedberg.at>
2018-11-18 10:40:28 +0100

    Remove unused grid

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 import {hot} from 'react-hot-loader';
 import {withStyles} from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
-import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -13,10 +12,10 @@ import {Routes} from './components';
 
 const styles = theme => ({
   container: {
-    marginLeft: theme.spacing.unit * 3,
-    marginRight: theme.spacing.unit * 3,
-    paddingLeft: theme.spacing.unit * 2,
-    paddingRight: theme.spacing.unit * 2,
+    marginLeft: theme.spacing.unit * 1,
+    marginRight: theme.spacing.unit * 1,
+    paddingLeft: theme.spacing.unit * 1,
+    paddingRight: theme.spacing.unit * 1,
     paddingTop: theme.spacing.unit,
     paddingBottom: theme.spacing.unit,
   },
@@ -28,24 +27,19 @@ const styles = theme => ({
 const App = ({classes}) => (
   <React.Fragment>
     <CssBaseline />
-    <Grid container spacing={24}>
-      <Grid item xs={12}>
-        <AppBar position="static">
-          <Toolbar>
-            <IconButton color="inherit" className={classes.menuButton}>
-              <MenuIcon />
-            </IconButton>
-
-            <Typography variant="h6" color="inherit">
-              Complete Elite Sweet Spreadsheet Suite
-            </Typography>
-          </Toolbar>
-        </AppBar>
-        <div className={classes.container}>
-          <Routes />
-        </div>
-      </Grid>
-    </Grid>
+    <AppBar position="static">
+      <Toolbar>
+        <IconButton color="inherit" className={classes.menuButton}>
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6" color="inherit">
+          Complete Elite Sweet Spreadsheet Suite
+        </Typography>
+      </Toolbar>
+    </AppBar>
+    <div className={classes.container}>
+      <Routes />
+    </div>
   </React.Fragment>
 );
 
