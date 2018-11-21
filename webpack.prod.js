397e3aa8079fbcfe2cf7d94c44b1428e1a41cd59
Erik Hedberg <erik@hedberg.at>
2018-11-21 20:07:13 +0100

    Move production specific to separate config

---
+++ webpack.prod.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  optimization: {
+    runtimeChunk: 'single',
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all',
+        },
+      },
+    },
+  },
+});
