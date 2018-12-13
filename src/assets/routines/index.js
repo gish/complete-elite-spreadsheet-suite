a7201c28a5fedc76fc053d1daf05792d579bb071
Erik Hedberg <erik@hedberg.at>
2018-12-13 15:46:07 +0100

    Add Bänkpress boogie

--- src/assets/routines/index.js
+++ src/assets/routines/index.js
@@ -2,8 +2,10 @@ import fiveThreeOne from './531.json';
 import nsunsBench from './nsuns-bench.json';
 import makeMommaProud from './make-momma-proud.json';
 import russianSquatRoutine from './russian-squat-routine.json';
+import bankpressBoogie from './bankpress-boogie.json';
 
 const routines = [
+  bankpressBoogie,
   russianSquatRoutine,
   //  fiveThreeOne,
   nsunsBench,
