bded2f25e5f8f176a78514f48029cc6c8a7c052b
Erik Hedberg <erik@hedberg.at>
2019-02-03 10:14:51 +0100

    Prevent duplicate exercises

--- bin/add-exercise.js
+++ bin/add-exercise.js
@@ -1,16 +1,23 @@
 const fs = require('fs');
+const R = require('ramda');
 const uuidv4 = require('uuid/v4');
 const EXERCISES_FILENAME = __dirname + '/../src/assets/exercises.json';
+const exercises = require(EXERCISES_FILENAME);
 
 const name = process.argv[2] || '';
+const id = uuidv4();
+const exercise = {id, name};
 
 if (name.length === 0) {
   console.error('Error: Missing exercise name');
   process.exit(1);
 }
-const id = uuidv4();
-const exercise = {id, name};
-const exercises = require(EXERCISES_FILENAME);
+
+if (R.any(e => e.name === exercise.name, exercises)) {
+  console.error('Error: Exercise already added');
+  process.exit(2);
+}
+
 const updatedExercises = [...exercises, exercise];
 
 fs.writeFileSync(EXERCISES_FILENAME, JSON.stringify(updatedExercises, null, 2));
