d491312cf9671b5cefa7ada2c18acff6e1ca131c
Erik Hedberg <erik@hedberg.at>
2019-02-21 14:24:29 +0100

    Add base routine edit page

--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -4,6 +4,7 @@ import {HashRouter as Router, Route, Link, Redirect} from 'react-router-dom';
 import {hot} from 'react-hot-loader';
 
 import * as cyclesPages from '../pages/cycles';
+import * as routinePages from '../pages/routines';
 
 const uuidV4 =
   '[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}';
@@ -21,6 +22,11 @@ class Routes extends React.Component {
             exact
             path={`/cycles/:id(${uuidV4})`}
             component={cyclesPages.Show}
+          />
+          <Route
+            exact
+            path={`/routines/edit/:id(${uuidV4})`}
+            component={routinePages.Edit}
           />
         </div>
       </Router>
---
+++ src/pages/routines/Edit/Edit.js
@@ -0,0 +1,7 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import {connect} from 'react-redux';
+
+const Edit = () => <div>Edit</div>;
+
+export default Edit;
