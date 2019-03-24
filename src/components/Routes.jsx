c0387490951e21d8b1bb37d88317a41f507356df
Erik Hedberg <erik@hedberg.at>
2019-03-24 10:43:01 +0100

    Convert Router to function component

--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -9,30 +9,26 @@ import * as routinePages from '../pages/routines';
 const uuidV4 =
   '[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}';
 
-class Routes extends React.Component {
-  render() {
-    return (
-      <Router>
-        <div>
-          <Route exact path="/" render={() => <Redirect to="/cycles" />} />
-          <Route exact path="/cycles" component={cyclesPages.List} />
-          <Route exact path="/cycles/create" component={cyclesPages.List} />
-          <Route exact path="/cycles/create" component={cyclesPages.Create} />
-          <Route
-            exact
-            path={`/cycles/:id(${uuidV4})`}
-            component={cyclesPages.Show}
-          />
-          <Route exact path={`/routines`} component={routinePages.List} />
-          <Route
-            exact
-            path={`/routines/edit/:id(${uuidV4})`}
-            component={routinePages.Edit}
-          />
-        </div>
-      </Router>
-    );
-  }
-}
+const Routes = () => (
+  <Router>
+    <div>
+      <Route exact path="/" render={() => <Redirect to="/cycles" />} />
+      <Route exact path="/cycles" component={cyclesPages.List} />
+      <Route exact path="/cycles/create" component={cyclesPages.List} />
+      <Route exact path="/cycles/create" component={cyclesPages.Create} />
+      <Route
+        exact
+        path={`/cycles/:id(${uuidV4})`}
+        component={cyclesPages.Show}
+      />
+      <Route exact path={`/routines`} component={routinePages.List} />
+      <Route
+        exact
+        path={`/routines/edit/:id(${uuidV4})`}
+        component={routinePages.Edit}
+      />
+    </div>
+  </Router>
+);
 
 export default hot(module)(Routes);
