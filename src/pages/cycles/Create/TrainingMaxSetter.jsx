90ccae5142e5deed930d1cdf82b1383637155a36
Erik Hedberg <erik@hedberg.at>
2019-03-24 11:09:46 +0100

    Convert TrainingMaxSetter to function component

--- src/pages/cycles/Create/TrainingMaxSetter.jsx
+++ src/pages/cycles/Create/TrainingMaxSetter.jsx
@@ -1,49 +1,41 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, {useState} from 'react';
+import * as R from 'ramda';
 import TextField from '@material-ui/core/TextField';
 import propTypes from '../../../proptypes';
 
-class TrainingMaxSetter extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      maxes: props.maxes,
-    };
-  }
-  change(id) {
-    return event => {
-      const newMaxValue = Number(event.target.value);
-      const newMaxes = this.state.maxes.map(max => {
-        if (max.id === id) {
-          return {...max, value: newMaxValue};
-        }
-        return max;
-      });
-      this.setState({maxes: newMaxes});
-      this.props.onUpdate(newMaxes);
-    };
-  }
+const TrainingMaxSetter = ({maxes, onUpdate}) => {
+  const [newMaxes, setMaxes] = useState(maxes);
+  const change = id => event => {
+    const newMaxValue = Number(event.target.value);
+    const isEqualId = id =>
+      R.pipe(
+        R.prop('id'),
+        R.equals(id),
+      );
+    R.pipe(
+      R.map(R.ifElse(isEqualId(id), R.assoc('value', newMaxValue), R.identity)),
+      R.juxt([setMaxes, onUpdate]),
+    )(newMaxes);
+  };
 
-  render() {
-    const {maxes} = this.state;
-    return (
-      <React.Fragment>
-        {maxes.map(max => (
-          <TextField
-            key={max.id}
-            label={max.name}
-            type="number"
-            onChange={this.change(max.id)}
-            defaultValue={max.value}
-            margin="dense"
-            fullWidth
-            required
-          />
-        ))}
-      </React.Fragment>
-    );
-  }
-}
+  return (
+    <React.Fragment>
+      {newMaxes.map(max => (
+        <TextField
+          key={max.id}
+          label={max.name}
+          type="number"
+          onChange={change(max.id)}
+          defaultValue={max.value}
+          margin="dense"
+          fullWidth
+          required
+        />
+      ))}
+    </React.Fragment>
+  );
+};
 
 TrainingMaxSetter.propTypes = {
   maxes: propTypes.trainingMaxes.isRequired,
