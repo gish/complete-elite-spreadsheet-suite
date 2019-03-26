78eb1d6d0bd0e216524c73982e37e692d2b28f64
Erik Hedberg <erik@hedberg.at>
2019-03-26 07:49:16 +0100

    Remove state handling for setting training maxes

--- src/pages/cycles/Create/TrainingMaxSetter.jsx
+++ src/pages/cycles/Create/TrainingMaxSetter.jsx
@@ -1,11 +1,10 @@
 import PropTypes from 'prop-types';
-import React, {useState} from 'react';
+import React from 'react';
 import * as R from 'ramda';
 import TextField from '@material-ui/core/TextField';
 import propTypes from '../../../proptypes';
 
 const TrainingMaxSetter = ({maxes, onUpdate}) => {
-  const [newMaxes, setMaxes] = useState(maxes);
   const change = id => event => {
     const newMaxValue = Number(event.target.value);
     const isEqualId = id =>
@@ -15,13 +14,13 @@ const TrainingMaxSetter = ({maxes, onUpdate}) => {
       );
     R.pipe(
       R.map(R.ifElse(isEqualId(id), R.assoc('value', newMaxValue), R.identity)),
-      R.juxt([setMaxes, onUpdate]),
-    )(newMaxes);
+      onUpdate,
+    )(maxes);
   };
 
   return (
     <React.Fragment>
-      {newMaxes.map(max => (
+      {maxes.map(max => (
         <TextField
           key={max.id}
           label={max.name}
